59281e4bb94423e5d024821e23502063b907ec4e
Andrei Musca <andreimusca@gmail.com>
2016-04-03 17:31:16 +0300

    Update entrypoint.sh
    default to server mode when no arguments are provided

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 DUPLICATI_CMD='mono /app/Duplicati.CommandLine.exe'
-
+DUPLICATI_SERVER_CMD='mono /app/Duplicati.Server.exe'
 if [ -z "$1" ]; then
     if [ ! -n "$DUPLICATI_PASS" ]; then
       echo "ERROR- DUPLICATI_PASS must be defined"
       exit 1
     fi
-	exec $DUPLICATI_CMD --webservice-port=8200 --webservice-interface=* --webservice-password=${DUPLICATI_PASS}
+	exec $DUPLICATI_SERVER_CMD --webservice-port=8200 --webservice-interface=* --webservice-password=${DUPLICATI_PASS}
 else
 	$DUPLICATI_CMD $@
 	if [ "$?" -eq 100 ] && [ -n "$ENABLE_AUTO_REPAIR" ]; then
